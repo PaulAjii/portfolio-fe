42b1cdf936ce4d0504bb4f55ecc1b07ed884c3bd
PaulAjii <paulajii134@gmail.com>
2025-04-10 22:06:25 +0100

    refactor: reset view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>This is Home</h1>
 </template>
